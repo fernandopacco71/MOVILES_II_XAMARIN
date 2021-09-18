565fc8b094cac9fe9e1fbe13c85ea3206155830e
luis fernando pacco condori <fernandopacco98@hotmail.com>
2021-09-18 09:54:26 -0500

    curso de moviles II icit

---
+++ apliacacion02/apliacacion02/apliacacion02/views/Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace apliacacion02.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        const double tasa = 19.11;
+        void Limpiar()
+        {
+            PesosEntry.Text = string.Empty;
+            DolaresLabel.Text = string.Empty;
+        }
+
+        private async void ConversorButton_Clicked(object sender, EventArgs e)
+        {
+            double pesos = 0, dolares = 0;
+
+
+            if (double.TryParse(PesosEntry.Text, out pesos))
+            {
+                dolares = pesos / tasa;
+                DolaresLabel.Text = $"{dolares:N2}";
+            }
+            else
+            {
+                await DisplayAlert("Error", "Cantidad no válida", "OK");
+                Limpiar();
+            }
+        }
+
+        private void LimpiarButton_Clicked(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
---
+++ miPrimerApp/miPrimerApp/miPrimerApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace miPrimerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //private async void Button_Clicked(object sender )
+        private async void BTNatras(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "bon atras", "ok");
+        }
+        private async void BTEnviar(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "Quieres enviar", "ok","Cancelar");
+        }
+        private async void OnActionSheetSimpleClicked(object sender, EventArgs e)
+        {
+            await DisplayActionSheet("ActionShellt: Send to?", "Cancel", null, "Email", "twiter", "Facebook");
+        }
+    }
+}
